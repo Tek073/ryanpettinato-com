0aed3c450c3d597a3dd404bbb56a4f61c65dc4c1
Ryan Pettinato <rnpettin@asu.edu>
2025-09-05 10:30:40 -0700

    update
    new book

--- scripts/webgl.js
+++ scripts/webgl.js
@@ -1,4 +1,10 @@
 
+function createShader(gl, type, source) 
+{
+    var shader = gl.createShader(type);
+    gl.shaderSource
+}
+
 document.addEventListener('DOMContentLoaded', function() {
         // Code to execute after the DOM is ready
         
